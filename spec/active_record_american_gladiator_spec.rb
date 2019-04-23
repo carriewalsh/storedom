2b30b1189a73b8f67f0ea36569d56e5f2222e255
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-04-22 19:01:56 -0600

    Refactors Breakthrough and Conquer

--- spec/active_record_american_gladiator_spec.rb
+++ spec/active_record_american_gladiator_spec.rb
@@ -83,7 +83,7 @@ describe "ActiveRecord American Gladiator" do
   end
 
   context "Breakthrough and Conquer" do
-    xit "returns all orders with Footballs and Wrestling Rings" do
+    it "returns all orders with Footballs and Wrestling Rings" do
       wrestling_ring = Item.create(name: "Wrestling Ring")
       football       = Item.create(name: "Football")
       sweat          = Item.create(name: "Sweat")
@@ -92,9 +92,9 @@ describe "ActiveRecord American Gladiator" do
       order_3        = Order.create(items: [football])
 
       # Changeable Start
-      orders = Order.all.select do |order|
-        order.items.include?(football) || order.items.include?(wrestling_ring)
-      end
+      orders = Order.joins(:items).select("orders.*").where("items.name = ? OR items.name = ?","Football", "Wrestling Ring")
+      # chosen = ["Football","Wrestling Ring"]
+      # orders = Order.joins(:items).select("orders.*").where("items.name": chosen) #which is better?
       # Changeable End
 
       # Hint: Take a look at the `Joins` section and the example that combines `joins` and `where` here: http://apidock.com/rails/ActiveRecord/QueryMethods/where
